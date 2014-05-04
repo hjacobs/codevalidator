8c718b617504f5030f3960df50187c4dca3ffe45
hjacobs <henning@jacobs1.de>
2014-05-04 13:19:25 +0200

    fix README file name in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.8',
     description='Simple source code validator with file reformatting option (remove trailing WS, pretty print XML, ..)'
         ,
-    long_description=open('README.md', 'r').read(),
+    long_description=open('README.rst', 'r').read(),
     author='Henning Jacobs',
     author_email='henning@jacobs1.de',
     url='https://github.com/hjacobs/codevalidator',
